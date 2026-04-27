61b0a527dee67aadd5416c3941fea670eec27b53
Jhone-cmd <jhone.1941@gmail.com>
2026-04-27 16:55:31 -0300

    receiving parameters from the controller in HTML.

--- src/main/java/br/com/jhonecmd/vacancy_management_front/PageFirst.java
+++ src/main/java/br/com/jhonecmd/vacancy_management_front/PageFirst.java
@@ -1,6 +1,7 @@
 package br.com.jhonecmd.vacancy_management_front;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PageFirst {
 
     @GetMapping("/home")
-    public String pageFirstHTML() {
+    public String pageFirstHTML(Model model) {
+
+        model.addAttribute("message", "Primeira mensagem vindo do controller");
         return "PageFirst";
     }
 
