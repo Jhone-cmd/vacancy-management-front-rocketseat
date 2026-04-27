383d81ac3e926f257d570db30cd4070dd943f767
Jhone-cmd <jhone.1941@gmail.com>
2026-04-27 16:37:36 -0300

    initial commit

---
+++ src/main/java/br/com/jhonecmd/vacancy_management_front/VacancyManagementFrontApplication.java
@@ -0,0 +1,13 @@
+package br.com.jhonecmd.vacancy_management_front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacancyManagementFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacancyManagementFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jhonecmd/vacancy_management_front/VacancyManagementFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jhonecmd.vacancy_management_front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacancyManagementFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
