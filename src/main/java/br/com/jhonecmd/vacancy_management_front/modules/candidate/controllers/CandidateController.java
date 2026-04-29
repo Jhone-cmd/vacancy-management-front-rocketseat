a38d924d0121d080696c827ced14b89e8217ad24
Jhone-cmd <jhone.1941@gmail.com>
2026-04-29 18:22:10 -0300

    restTemplate for external API access part-1.

--- src/main/java/br/com/jhonecmd/vacancy_management_front/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/jhonecmd/vacancy_management_front/modules/candidate/controllers/CandidateController.java
@@ -3,6 +3,8 @@ package br.com.jhonecmd.vacancy_management_front.modules.candidate.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequestMapping("/candidate")
@@ -12,4 +14,17 @@ public class CandidateController {
     public String LoginCandidate() {
         return "modules/candidate/login";
     }
+
+    @PostMapping("/singIn")
+    public String singIn(RedirectAttributes redirectAttributes, String email, String password) {
+
+        if (email.equals("johndoe@email.com")) {
+            return "modules/candidate/profile";
+        }
+
+        redirectAttributes.addFlashAttribute("error", "Credenciais Inválidas");
+
+        return "redirect:/candidate/login";
+    }
+
 }
